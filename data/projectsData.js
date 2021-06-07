204794f2f0f3215b56b4f4ec5d02dd7d50a8fc4e
mojtaba <m.yazdanpanah@gmail.com>
2021-06-07 06:52:54 +0430

    update data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,34 +1,32 @@
 const projectsData = [
   {
     title: 'Shoor App Design',
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    description: `Shoor is a social media application that is based on the Instagram and Spotify. One app for listening to music, make playlists, follow artists and watch their live performances.
+`,
     imgSrc: '/static/images/Shoor.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'PassiOne Web Design',
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/Coach.png',
-    href: 'https://www.google.com',
+    href: 'http://localhost:3000/blog/project-case-studies/shoor-case-study',
   },
   {
     title: 'Housemart App Design',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/Exper.png',
-    href: '/blog/the-time-machine',
+    description: `Housmart is a hub that help users th control security, light, green life, electronics, pets and the heat system with the help of A.I.
+`,
+    imgSrc: '/static/images/Housemart.png',
+    href: '/http://localhost:3000/blog/project-case-studies/housmart-case-study',
+  },
+  {
+    title: 'PassiOne Web Design',
+    description: `Life coach to guide us through some of the most tumultuous times and steer us in the 
+right direction to achieve our goals and our dreams. It’s difficult to see things from a fresh 
+perspective and realize what you’re capable of without them. PassiOne is a platform for getting life coaches`,
+    imgSrc: '/static/images/Coach.png',
+    href: 'https://www.google.com',
   },
   {
     title: 'Exper Web Design',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/Housemart.png',
+    imgSrc: '/static/images/Exper.png',
     href: '/blog/the-time-machine',
   },
   {
